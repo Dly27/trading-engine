ad25d5e5ed4de3f1b6a90f0ecee29a688f0cf477
Dly27 <149893105+Dly27@users.noreply.github.com>
2025-08-06 22:42:22 +0100

    Fixed empty price node issue

--- order_book/order_book.py
+++ order_book/order_book.py
@@ -4,6 +4,10 @@ from typing import Literal
 
 class Node:
     def __init__(self, price, parent=None, left=None, right=None):
+        """
+        :param price: The price of the node
+        values: OrderedDict storing Order objects in the form "order_id" : Order
+        """
         self.parent = parent
         self.left = left
         self.right = right
@@ -20,6 +24,11 @@ class RedBlackTree:
         self.type = type
 
     def add_price(self, price):
+        """
+        Adds a price node to the tree
+        Sets the new node as a leaf
+        Re-balances tree so node is in correct position
+        """
         if self.root is None:
             self.root = Node(price=price)
             self.root.colour = "black"
@@ -88,6 +97,10 @@ class RedBlackTree:
         node.parent = left_child
 
     def rebalance_red_black(self, node):
+        """
+        Re-balances tree
+        :param node: Node to move to the correct position
+        """
         while node != self.root and node.parent.colour == "red":
             if node.parent == node.parent.parent.left:
                 uncle = node.parent.parent.right
@@ -129,48 +142,95 @@ class RedBlackTree:
         self.root.colour = "black"
 
     def search_price(self, price):
-        current = self.root
+        """
+        Search for a price node of a given price
+        :param price: Price of price node to find
+        :return:
+        """
+        current_price_node = self.root
 
-        while current is not None:
-            if current.price == price:
-                return current
+        while current_price_node is not None:
+            if current_price_node.price == price:
+                return current_price_node
 
-            if price < current.price:
-                current = current.left
+            if price < current_price_node.price:
+                current_price_node = current_price_node.left
             else:
-                current = current.right
+                current_price_node = current_price_node.right
 
-        return None
+        return current_price_node
 
     def get_best_bid(self):
+        """
+        Return the order with the best bid
+        :return: Order
+        """
         if self.type != "bids":
             raise ValueError("INVALID: THIS IS FOR BIDS")
 
         if self.root is None:
             return None
 
-        current = self.root
+        current_price_node = self.root
 
-        while current.right is not None:
-            current = current.right
+        # Find node furthest to the right
+        while current_price_node.right is not None:
+            current_price_node = current_price_node.right
 
-        first_order_id = next(iter(current.values))
-        return current.values[first_order_id]
+        # Traverse backwards
+        while current_price_node is not None:
+            if len(current_price_node.values) > 0:
+                first_order_id = next(iter(current_price_node.values))
+                return current_price_node.values[first_order_id]
+
+            if current_price_node.left is not None:
+                current_price_node = current_price_node.left
+                while current_price_node.right is not None:
+                    current_price_node = current_price_node.right
+            else:
+                parent = current_price_node.parent
+                while parent is not None and current_price_node == parent.left:
+                    current_price_node = parent
+                    parent = parent.parent
+                current_price_node = parent
+
+        return None
 
     def get_best_ask(self):
+        """
+        Return the order with the best ask
+        :return: Order
+        """
         if self.type != "asks":
             raise ValueError("INVALID: THIS IS FOR ASKS")
 
         if self.root is None:
             return None
 
-        current = self.root
+        current_price_node = self.root
 
-        while current.left is not None:
-            current = current.left
+        # Find node furthest to the left
+        while current_price_node.left is not None:
+            current_price_node = current_price_node.left
 
-        first_order_id = next(iter(current.values))
-        return current.values[first_order_id]
+        # Traverse forwards until we find orders
+        while current_price_node is not None:
+            if len(current_price_node.values) > 0:
+                first_order_id = next(iter(current_price_node.values))
+                return current_price_node.values[first_order_id]
+
+            if current_price_node.right is not None:
+                current_price_node = current_price_node.right
+                while current_price_node.left is not None:
+                    current_price_node = current_price_node.left
+            else:
+                parent = current_price_node.parent
+                while parent is not None and current_price_node == parent.right:
+                    current_price_node = parent
+                    parent = parent.parent
+                current_price_node = parent
+
+        return None
 
 
 class Order:
@@ -188,20 +248,30 @@ class OrderBook:
     def __init__(self):
         self.asks = RedBlackTree(type="asks")
         self.bids = RedBlackTree(type="bids")
-        self.order_id_map = {}
+        self.order_id_map = {} # order_id: price_node
 
     def add_order(self, order):
+        """
+        Adds order to either bids or asks
+        Adds order to order_id_map
+        :param order:
+        :return:
+        """
         if order.order_type == "ask":
-            price_node = self.asks.add_price(order.order_price)  # Create new price level
+            price_node = self.asks.add_price(order.order_price)  # Add new price node
             price_node.values[order.order_id] = order
             self.order_id_map[order.order_id] = price_node
 
         elif order.order_type == "bid":
-            price_node = self.bids.add_price(order.order_price)  # Create new price level
+            price_node = self.bids.add_price(order.order_price)  # Add new price node
             price_node.values[order.order_id] = order
             self.order_id_map[order.order_id] = price_node
 
     def cancel_order(self, order_id):
+        """
+        Deletes order from bids or asks
+        Deletes order from order_id_map
+        """
         price_node = self.order_id_map.get(order_id)
 
         if price_node is None:
@@ -214,12 +284,22 @@ class OrderBook:
             del self.order_id_map[order_id]
 
     def get_best_bid(self):
+        """
+        Get the best bid
+        """
         return self.bids.get_best_bid()
 
     def get_best_ask(self):
+        """
+        Get the worst bid
+        :return:
+        """
         return self.asks.get_best_ask()
 
     def get_spread(self):
+        """
+        Get the spread
+        """
         best_bid = self.get_best_bid()
         best_ask = self.get_best_ask()
 
@@ -243,6 +323,12 @@ if __name__ == "__main__":
         order = Order(order_id=str(counter), order_type="ask", order_price=price)
         book.add_order(order=order)
         counter += 1
+
+    best_bid = book.get_best_bid()
+    book.cancel_order(order_id=best_bid.order_id)
+
+    second_best_bid = book.get_best_bid()
+    book.cancel_order(order_id=second_best_bid.order_id)
 
     print(f"Spread: {book.get_spread()}")
     print(f"Best bid: {book.get_best_bid().order_price}")
